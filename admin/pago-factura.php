4ae8c715591c60a944663e2abee449a4d6ec6bd1
11179607 <proyectosmceaa@gmail.com>
2026-03-19 12:38:12 -0500

    factenpdf

--- admin/pago-factura.php
+++ admin/pago-factura.php
@@ -376,8 +376,7 @@ if ($modo === 'pdf') {
     $invoice = invoice_number($payment);
     while (ob_get_level()) { ob_end_clean(); }
     header('Content-Type: application/pdf');
-    header('Content-Disposition: attachment; filename="' . $invoice . '.pdf"');
-    header('Content-Length: ' . strlen($pdf));
+    header('Content-Disposition: inline; filename="' . $invoice . '.pdf"');
     header('Cache-Control: private, max-age=0, must-revalidate');
     header('Pragma: public');
     echo $pdf;
